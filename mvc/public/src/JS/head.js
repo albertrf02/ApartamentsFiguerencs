9cf7ba2537f9e841028bcccd3eb32a17306bf083
Jordi <jrodriguez4@cendrassos.net>
2023-10-19 15:55:30 +0200

    Importants

--- mvc/public/src/JS/head.js
+++ mvc/public/src/JS/head.js
@@ -12,3 +12,19 @@ function agregarClaseDark() {
     }
     console.log('Alternando clase dark');
 }
+const nav = document.getElementById('nav');
+  let lastScrollTop = 0;
+
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+      // Scrolling down, hide the nav
+      nav.classList.add('hidden');
+    } else {
+      // Scrolling up, show the nav
+      nav.classList.remove('hidden');
+    }
+
+    lastScrollTop = scrollTop;
+  });
