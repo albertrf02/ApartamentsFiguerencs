fe587eee7905dda96c4e84781293a398ae8d2206
Albert Rocas <albertrocas02@gmail.com>
2023-10-28 11:33:07 +0200

    Arreglo login

--- mvc/src/controllers/login.php
+++ mvc/src/controllers/login.php
@@ -2,21 +2,20 @@
 function ctrlLogin($request, $response, $container)
 {
     $response->setTemplate("login.php");
-    return $response;    
-    
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Handle form submission
         if (isset($_POST['email']) && isset($_POST['password'])) {
             // Get user input
             $email = $_POST['email'];
             $password = $_POST['password'];
-    
+
             // Create an instance of the model
-            $LoginUser = new LoginUser($pdo);
-    
+            $LoginModel = $container->loginUser();
+
             // Call the loginUser method from the model
-            $result = $LoginUser->loginUser($email, $password);
-    
+            $result = $LoginModel->loginUser($email, $password);
+
             if ($result) {
                 $_SESSION['user'] = $result;
                 header('Location: index.php');
@@ -25,4 +24,6 @@ function ctrlLogin($request, $response, $container)
             }
         }
     }
+    return $response;
 }
+
