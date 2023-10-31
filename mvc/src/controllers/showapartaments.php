1d92ce509ad171fccc717fc8d2b680ecf6391ad7
Jordi <jrodriguez4@cendrassos.net>
2023-10-31 16:40:10 +0100

    cerca de apartaments

--- mvc/src/controllers/showapartaments.php
+++
@@ -1,9 +0,0 @@
-<?php
-function showApartaments($request, $response, $container){
-    $apartaments = $container->$apartaments()->getAllApartaments($datepicker, $datepicker2, $numPersones);
-    $response->set("apartaments", $apartaments);
-    return $response;
-
-};
-
-
