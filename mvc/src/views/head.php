cf41943ea47ec8910bc2e9a1b17337d04985c2b8
Jordi <jrodriguez4@cendrassos.net>
2023-10-31 16:56:39 +0100

    xd

--- mvc/src/views/head.php
+++ mvc/src/views/head.php
@@ -1,26 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
-  <link href='src/CSS/index.css' rel='stylesheet' />
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  <title>Kalewi</title>
-</head>
-
-<body>
-  <nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand">LOGO</a>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-success search" type="submit">Ss</button>
-        <a class="btn btn-outline-success" href="index.php?r=login">Entrar</a>
-        <a class="btn btn-outline-success" href="index.php?r=registre">Crear</a>
-        <a class="btn btn-outline-success" href="index.php?r=apartament">apartament</a>
-        <a onclick="agregarClaseDark()" class="btn btn-outline-success">Canviar</a>
-      </form>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link href='src/CSS/index.css' rel='stylesheet' />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Kalewi</title>
+  </head>
+  <body>
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand">LOGO</a>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success search" type="submit">Ss</button>
+          <a class="btn btn-outline-success" href="index.php?r=login">Entrar</a>
+          <a class="btn btn-outline-success" href="index.php?r=registre">Crear</a>
+          <a class="btn btn-outline-success" href="index.php?r=apartament">apartament</a>
+          <a onclick="agregarClaseDark()" class="btn btn-outline-success" >Canviar</a>
+        </form>
+      </div>
+    </nav>
+    <hr class="hr" />
     </div>
 </html>
