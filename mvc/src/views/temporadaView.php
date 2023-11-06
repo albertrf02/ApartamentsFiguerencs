8f10c07e7122e7646d0874a7036070b8847ab3fe
Albert Rocas <albertrocas02@gmail.com>
2023-11-07 00:18:47 +0100

    arreglo i boto reset form temporada

--- mvc/src/views/temporadaView.php
+++ mvc/src/views/temporadaView.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="src/CSS/registre.css">
+    <script src="src/JS/Apartaments.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Login</title>
+    <title>Temporada</title>
 </head>
 
 <body>
@@ -29,7 +30,6 @@
                     </div>
                     <h2 class="text-center">Canvi Temporada</h2>
                     <hr>
-                    <!-- UpdateSeasonForm.php -->
                     <form action="index.php?r=temporada" method="post">
 
                         <label for="high_season_start">Començament temporada alta:</label>
@@ -53,7 +53,7 @@
                         <br>
 
                         <input type="submit" name="update_season" value="Update Season Dates">
-                        <input type="reset" value="Reset">
+                        <button type="button" id="resetToDefault">Reset to Default</button>
                     </form>
 
                 </div>
