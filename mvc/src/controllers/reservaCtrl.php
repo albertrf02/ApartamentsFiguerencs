2496dcc7518d9b41b9fe383853460c642f07b558
Albert Rocas <albertrocas02@gmail.com>
2023-11-09 16:30:22 +0100

    dades pdf

--- mvc/src/controllers/reservaCtrl.php
+++ mvc/src/controllers/reservaCtrl.php
@@ -3,13 +3,19 @@ function ctrlReserva($request, $response, $container)
 {
     if (isset($_REQUEST["action"]) && $_REQUEST["action"] === "descarregarPDF" && isset($_REQUEST["Id"])) {
         $id = $_REQUEST["Id"];
+        $nom = $_SESSION["user"]["Nom"];
+        $cognom = $_SESSION["user"]["Cognoms"];
+        $email = $_SESSION["user"]["CorreuElectronic"];
         $response->setTemplate("descarregarPDF.php");
         $response->set("id", $id);
+        $response->set("nom", $nom);
+        $response->set("cognom", $cognom);
+        $response->set("email", $email);
         error_log("Descarregant PDF de la reserva " . $id);
         return $response;
     }
 
-    
+
 
 
     return $response;
