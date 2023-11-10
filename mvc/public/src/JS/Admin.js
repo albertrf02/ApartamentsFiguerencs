4bb979d0ac38e8ef4f36dae118636a3bec8d1ce6
Albert Rocas <albertrocas02@gmail.com>
2023-11-10 18:18:38 +0100

    contrasenya

--- mvc/public/src/JS/Admin.js
+++ mvc/public/src/JS/Admin.js
@@ -1,7 +1,9 @@
-document.getElementById('resetToDefault').addEventListener('click', function () {
-    document.getElementById('high_season_start').value = '2023-01-01';
-    document.getElementById('high_season_end').value = '2023-06-30';
-    document.getElementById('low_season_start').value = '2023-07-01';
-    document.getElementById('low_season_end').value = '2023-12-31';
+// Default dates
+document
+  .getElementById("resetToDefault")
+  .addEventListener("click", function () {
+    document.getElementById("high_season_start").value = "2023-01-01";
+    document.getElementById("high_season_end").value = "2023-06-30";
+    document.getElementById("low_season_start").value = "2023-07-01";
+    document.getElementById("low_season_end").value = "2023-12-31";
   });
-  
