f755e90431d9d0697627e7cf7fb78fb6ac8ee1ee
Albert Rocas <albertrocas02@gmail.com>
2023-11-10 12:21:18 +0100

    buscar rserva

--- mvc/src/views/buscarReserva.php
+++ mvc/src/views/buscarReserva.php
@@ -4,28 +4,123 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Reservation Search</title>
+    <style>
+        @import url("https://fonts.cdnfonts.com/css/gabarito");
+
+        body {
+            font-family: 'Gabarito', sans-serif;
+            /* Change 'Gabarito' to the correct font name */
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 20px auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .form-group {
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        input {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .button1 {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            font-weight: bold;
+            text-align: center;
+            text-decoration: none;
+            background-color: #ADD8E6;
+            /* Light Blue Color */
+            color: #000;
+            /* Black Text */
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        h2 {
+            margin-top: 20px;
+            color: #333;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th,
+        td {
+            padding: 12px;
+            border: 1px solid #ddd;
+            text-align: left;
+        }
+
+        th {
+            background-color: #ADD8E6;
+            color: #000;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        tr:hover {
+            background-color: #f1f1f1;
+        }
+
+        p.error {
+            color: #ff0000;
+            font-weight: bold;
+        }
+    </style>
 </head>
 
 <body>
     <form method="POST" action="index.php?r=buscarreserva">
         <div class="form-group">
-            <label for="data">Data</label>
-            <input type="date" class="form-control" id="data" name="data">
+            <label for="data">Date</label>
+            <input type="date" id="data" name="data">
         </div>
         <div class="text-center">
-            <button type="submit" class="button1">Submit</button>
+            <button type="submit" class="button1">Search</button>
         </div>
     </form>
+
     <?php if (isset($reserves) && !empty($reserves)): ?>
         <h2>Reserves:</h2>
         <table>
             <thead>
                 <tr>
-                    <th>Reservation ID</th>
-                    <th>Title</th>
-                    <th>User Name</th>
-                    <th>User Surname</th>
+                    <th>ID de la Reserva</th>
+                    <th>Apartament</th>
+                    <th>Nom de l'Usuari</th>
+                    <th>Cognom de l'usuari</th>
                 </tr>
             </thead>
             <tbody>
@@ -48,7 +143,7 @@
             </tbody>
         </table>
     <?php elseif (isset($error)): ?>
-        <p>
+        <p class="error">
             <?php echo $error; ?>
         </p>
     <?php endif; ?>
